54b70ddb0f7b58823a11bd863a07bba31f128aa8
Matthew Batchelder <borkweb@gmail.com>
2013-01-09 15:31:20 -0500

    Adding CodeSniffer and sniff files

---
+++ go-code-scanner.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name: GigaOM Code Scanner
+Version: 0.1
+Plugin URI: http://gigaom.com
+Description: Scan theme/plugin code for GigaOM standards
+Author: GigaOM Network
+Author URI: http://gigaom.com
+Contributors: borkweb, zbtirrell
+Tags: standards
+Tested up to: 3.4.1
+Stable tag: 3.4
+License: MIT
+License URI: http://opensource.org/licenses/mit-license.php
+*/
